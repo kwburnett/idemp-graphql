7787c29c6ef4b1f9791bad491458b4ed43cc7c28
Kevin <kwburnett@gmail.com>
2020-10-14 17:39:22 +0100

    successfully added GraphQL servlet

--- src/org/bandahealth/idempiere/graphql/GraphQLEndpoint.java
+++ src/org/bandahealth/idempiere/graphql/GraphQLEndpoint.java
@@ -1,13 +1,15 @@
 package org.bandahealth.idempiere.graphql;
 
+import org.compiere.util.CLogger;
+
 import graphql.kickstart.servlet.GraphQLConfiguration;
 import graphql.kickstart.servlet.GraphQLHttpServlet;
 import graphql.kickstart.tools.SchemaParser;
 import graphql.schema.GraphQLSchema;
 
-import javax.servlet.annotation.WebServlet;
-
 public class GraphQLEndpoint extends GraphQLHttpServlet {
+
+	private final CLogger logger = CLogger.getCLogger(GraphQLEndpoint.class);
 
 	@Override
 	protected GraphQLConfiguration getConfiguration() {
@@ -17,7 +19,7 @@ public class GraphQLEndpoint extends GraphQLHttpServlet {
 	private GraphQLSchema createSchema() {
 		LinkRepository linkRepository = new LinkRepository();
 		return SchemaParser.newParser()
-				.file("schema.graphqls")
+				.file("WEB-INF/resources/schema.graphqls")
 				.resolvers(new Query(linkRepository))
 				.build()
 				.makeExecutableSchema();
