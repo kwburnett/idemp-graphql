5f4815f9c7486699a85e560e981313493e75b9bb
Kevin <kwburnett@gmail.com>
2020-10-17 10:44:37 +0100

    updating how the unauthenticated messages are displayed

--- src/org/bandahealth/idempiere/graphql/filter/AuthenticationFilter.java
+++ src/org/bandahealth/idempiere/graphql/filter/AuthenticationFilter.java
@@ -1,6 +1,9 @@
 package org.bandahealth.idempiere.graphql.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,19 +11,20 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletResponse;
 import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.HttpHeaders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bandahealth.idempiere.graphql.utils.AuthenticationUtil;
+import org.bandahealth.idempiere.graphql.utils.StringUtil;
 import org.compiere.util.Env;
 import org.compiere.util.Util;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
 
 /**
- * Basic Authentication
+ * Basic Authentication on all requests
  *
  * @author kevin
  */
@@ -28,18 +32,42 @@ public class AuthenticationFilter implements Filter {
 
 	private final String ERROR_UNAUTHORIZED = "Unauthorized";
 	private final String ERROR_INTERNAL_SERVER_ERROR = "Internal Server Error";
+	private final List<String> ALLOWABLE_UNAUTHENTICATED_QUERIES = new ArrayList<String>() {{
+		add("signIn");
+	}};
 
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException { }
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequestWrapper bandaRequest = new BandaServletRequestWrapper(request);
-		String requestBody = bandaRequest.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+		String requestQuery = bandaRequest.getParameter("query");
+		// If the query parameter didn't come through, try to pull it from the body
+		if (StringUtil.isNullOrEmpty(requestQuery)) {
+			try {
+				String requestBody = bandaRequest.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+				requestQuery = (String) new ObjectMapper().readValue(requestBody, HashMap.class).get("query");
+			} catch (Exception ignore) {
+			}
+			if (StringUtil.isNullOrEmpty(requestQuery)) {
+				requestQuery = "";
+			}
+		}
 		// Don't filter a request to get an authentication session or to change a password
-		if (bandaRequest.getMethod().equals(HttpMethod.POST) && (requestBody.contains("signIn("))) {
-			chain.doFilter(bandaRequest, response);
-			return;
+		if (bandaRequest.getMethod().equals(HttpMethod.POST)) {
+			boolean requestCanProceedWithoutAuthentication = false;
+			for (String allowableUnauthenticatedQuery : ALLOWABLE_UNAUTHENTICATED_QUERIES) {
+				if (requestQuery.contains(allowableUnauthenticatedQuery + "(")) {
+					requestCanProceedWithoutAuthentication = true;
+					break;
+				}
+			}
+			if (requestCanProceedWithoutAuthentication) {
+				chain.doFilter(bandaRequest, response);
+				return;
+			}
 		}
 
 		String authHeaderVal = bandaRequest.getHeader(HttpHeaders.AUTHORIZATION);
@@ -50,18 +78,18 @@ public class AuthenticationFilter implements Filter {
 				AuthenticationUtil.validate(authHeaderVal.split(" ")[1]);
 				if (Util.isEmpty(Env.getContext(Env.getCtx(), Env.AD_USER_ID))
 						|| Util.isEmpty(Env.getContext(Env.getCtx(), Env.AD_ROLE_ID))) {
-					abortRequest(response, ERROR_UNAUTHORIZED);
+					abortRequest(requestQuery, response, ERROR_UNAUTHORIZED);
 					return;
 				}
 			} catch (JWTVerificationException ex) {
-				abortRequest(response, ERROR_UNAUTHORIZED);
+				abortRequest(requestQuery, response, ERROR_UNAUTHORIZED);
 				return;
 			} catch (Exception ex) {
-				abortRequest(response, ERROR_INTERNAL_SERVER_ERROR);
+				abortRequest(requestQuery, response, ERROR_INTERNAL_SERVER_ERROR);
 				return;
 			}
 		} else {
-			abortRequest(response, ERROR_UNAUTHORIZED);
+			abortRequest(requestQuery, response, ERROR_UNAUTHORIZED);
 			return;
 		}
 
@@ -69,10 +97,38 @@ public class AuthenticationFilter implements Filter {
 	}
 
 	@Override
-	public void destroy() { }
+	public void destroy() {
+	}
 
-	private void abortRequest(ServletResponse response, String errorMessage) throws IOException {
+	/**
+	 * Write a response body as close as possible to what GraphQL generates
+	 *
+	 * @param requestQuery The query string passed in as part of the request
+	 * @param response     The response to return
+	 * @param errorMessage The error message to use
+	 * @throws IOException
+	 */
+	private void abortRequest(String requestQuery, ServletResponse response, String errorMessage) throws IOException {
+		List<String> paths = new ArrayList<>();
+		if (!StringUtil.isNullOrEmpty(requestQuery)) {
+			try {
+				// Try to get the requested queries so they can be returned in the error
+				paths.add("\"" + requestQuery.replace("mutation", "")
+						.replace(":", " ").replace("(", " ").replace("{", " ")
+						.split(" ")[1] + "\"");
+			} catch (Exception ignore) {
+			}
+		}
 		response.setContentType("application/json");
-		response.getWriter().write("{ \"data\": {}, \"errors\": [ \"" + errorMessage + "\" ] }");
+		String errorResponse = "{\"data\":{";
+		if (!paths.isEmpty()) {
+			errorResponse += paths.stream().reduce("", (accumulator, path) -> accumulator + path + ": null,");
+		}
+		errorResponse += "},\"errors\":[{\"message\": \"" + errorMessage + "\"";
+		if (!paths.isEmpty()) {
+			errorResponse += ",\"paths\":[" + String.join(",", paths) + "]";
+		}
+		errorResponse += "}}";
+		response.getWriter().write(errorResponse);
 	}
 }
