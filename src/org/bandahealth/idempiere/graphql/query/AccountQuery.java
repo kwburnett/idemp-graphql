113af823deed244ff0ec182f84346ab14ca2b250
Kevin <kwburnett@gmail.com>
2020-11-04 08:43:48 +0100

    adding individual account fetching

--- src/org/bandahealth/idempiere/graphql/query/AccountQuery.java
+++ src/org/bandahealth/idempiere/graphql/query/AccountQuery.java
@@ -2,16 +2,26 @@ package org.bandahealth.idempiere.graphql.query;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.schema.DataFetchingEnvironment;
+import org.bandahealth.idempiere.graphql.dataloader.impl.AccountDataLoader;
 import org.bandahealth.idempiere.graphql.model.Connection;
 import org.bandahealth.idempiere.graphql.model.PagingInfo;
 import org.bandahealth.idempiere.graphql.repository.AccountRepository;
 import org.compiere.model.MElementValue;
+import org.dataloader.DataLoader;
+
+import java.util.concurrent.CompletableFuture;
 
 public class AccountQuery implements GraphQLQueryResolver {
 	private final AccountRepository accountRepository;
 
 	public AccountQuery() {
 		accountRepository = new AccountRepository();
+	}
+
+	public CompletableFuture<MElementValue> account(String id, DataFetchingEnvironment environment) {
+		final DataLoader<String, MElementValue> dataLoader = environment.getDataLoaderRegistry()
+				.getDataLoader(AccountDataLoader.ACCOUNT_BY_UUID_DATA_LOADER);
+		return dataLoader.load(id);
 	}
 
 	public Connection<MElementValue> accounts(String filter, String sort, int page, int pageSize,
